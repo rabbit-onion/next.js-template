c002fd4826d63ff35d4b6817b37f5898f7e51109
Onion <csr1000@naver.com>
2024-11-27 13:18:53 +0900

    header layout

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,17 @@
+'use client';
+
+import { Container, Flex, Heading } from '@chakra-ui/react';
 import React from 'react';
 
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header>
+      <Container className='flex justify-between items-center'>
+        <Heading as='h1'>logo</Heading>
+        <nav>nav</nav>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
